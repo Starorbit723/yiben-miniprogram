018cfe4c73937459624eabf510d88cb4e9d4f049
sca <happysca@gmail.com>
2025-04-13 19:02:25 +0800

    校区信息读写

---
+++ cloudfunctions/operations/index.js
@@ -0,0 +1,37 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk');
+
+const schoolRead = require('./schoolRead/index');
+const schoolSave = require('./schoolSave/index');
+
+cloud.init({
+  env: "cloud1-0gvvdaq4c40b8f74"
+});
+
+const db = cloud.database();
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  try {
+    // 参数基础校验
+    if (!event.type || !['schoolRead', 'schoolSave'].includes(event.type)) {
+      return { code: 400, message: '无效的操作类型' };
+    }
+
+    // 路由处理
+    switch (event.type) {
+      case 'schoolRead':
+        return await schoolRead.main(event.data, db);
+      case 'schoolSave':
+        return await schoolSave.main(event.data, db);
+      default:
+        return { code: 400, message: '未实现的操作类型' };
+    }
+  } catch (error) {
+    console.error('全局错误:', error);
+    return { 
+      code: error.code || 500, 
+      message: error.message || '服务器内部错误'
+    };
+  }
+}
--- miniprogram/pages/testPage/index.js
+++ miniprogram/pages/testPage/index.js
@@ -20,8 +20,49 @@ Page({
     })
   },
   testFuction2() {
+    wx.cloud.callFunction({
+      name: 'operations',
+      data: {
+        type: 'schoolRead',
+        data: {
+          schoolid: 'haidian_1'
+        }
+      }
+    }).then(res => {
+      console.log('schoolRead result:', res)
+    }).catch(err => {
+      console.error('schoolRead error:', err)
+    })
     console.log('testFuction2 start');
   },
+  testFuction3() {
+    wx.cloud.callFunction({
+      name: 'operations',
+      data: {
+        type: 'schoolSave',
+        data: {
+          schoolid: 'haidian_2',
+          detail: {
+            area:"1000",
+            code:"123",
+            address:"haidian",
+            name:"海淀校区",
+            phone:"12345678901",
+            email:"12345678901@163.com",
+            website:"www.baidu.com",
+            logo:"https://www.baidu.com/logo.png",
+            description:"海淀校区"
+          }
+        }
+      }
+    }).then(res => {
+      console.log('schoolSave result:', res)
+    }).catch(err => {
+      console.error('schoolSave error:', err)
+    })
+    console.log('testFuction3 start');
+  },
+  
   /**
    * 生命周期函数--监听页面加载
    */
