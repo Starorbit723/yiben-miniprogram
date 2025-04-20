69d85f6343ce53ff04491dbf83095c663c96edd4
sca <happysca@gmail.com>
2025-04-20 14:53:18 +0800

    不检测数据更新数量

--- cloudfunctions/operations/bookModify/index.js
+++ cloudfunctions/operations/bookModify/index.js
@@ -66,19 +66,11 @@ exports.main = async (params, db) => {
         data: updateData
       });
     
-    if (updateResult.stats.updated > 0) {
-      return {
-        success: true,
-        message: '预约信息更新成功',
-        updated: updateResult.stats.updated
-      };
-    } else {
-      return {
-        success: false,
-        errMsg: '预约信息更新失败',
-        updated: 0
-      };
-    }
+    return {
+      success: true,
+      message: '预约信息更新成功',
+      updated: updateResult.stats.updated
+    };
     
   } catch (err) {
     return {
--- cloudfunctions/operations/userInfoAuto/index.js
+++ cloudfunctions/operations/userInfoAuto/index.js
@@ -76,19 +76,11 @@ exports.main = async (params, db) => {
         data: updateData
       });
     
-    if (updateResult.stats.updated > 0) {
-      return {
-        success: true,
-        message: '用户信息更新成功',
-        updated: updateResult.stats.updated
-      };
-    } else {
-      return {
-        success: false,
-        errMsg: '用户信息更新失败',
-        updated: 0
-      };
-    }
+    return {
+      success: true,
+      message: '用户信息更新成功',
+      updated: updateResult.stats.updated
+    };
   } catch (err) {
     return {
       success: false,
