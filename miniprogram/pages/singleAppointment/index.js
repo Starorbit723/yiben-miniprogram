b6a886dcb19c2b8e0144ede6e7101fd06a54bb00
fangyangyadong <yadong723@sohu.com>
2025-04-27 17:47:31 +0800

    tj

--- miniprogram/pages/singleAppointment/index.js
+++ miniprogram/pages/singleAppointment/index.js
@@ -3,11 +3,12 @@ const app = getApp();
 
 Page({
   data: {
+    bookid: '',
     form: {
-      parentName: '',
-      studentName: '',
-      phoneNumber: '',
-      school: 0
+      ownerName: '',
+      ownerChildren: '',
+      ownerPhone: '',
+      schoolid: 0
     },
     schoolOptions: [
       {id: 0 , name: '广安门校区'}
@@ -21,17 +22,48 @@ Page({
       name: '广益大厦'
     }],
   },
-  openModal() {
-    this.setData({
-      showModal: true
-    });
-  },
-  closeModal() {
-    this.setData({
-      showModal: false
-    });
-  },
   submitSigleAppointment() {
-    console.log(app.globalData);
+    console.log('bookMain call', this.data.form);
+    wx.cloud.callFunction({
+      name: 'operations',
+      data: {
+        type: 'bookMain',
+        data: {
+          yibenid: app.globalData.userInfo.yibenid,
+          openid: app.globalData.userInfo.openid,
+          bookType: 1,
+          originOpenid: app.globalData.userInfo.openid,
+          prevOpenid: app.globalData.userInfo.openid,
+          ownerName: this.data.form.ownerName,
+          ownerPhone: this.data.form.ownerPhone,
+          ownerChildren: this.data.form.ownerChildren,
+          schoolid: this.data.form.schoolid,
+        },
+      }
+    }).then(res => {
+      console.log('userInfoAuto result:', res);
+      if (res.result.success) {
+        this.setData({
+          bookid: res.result.bookid
+        });
+      }
+    }).catch(err => {
+      console.error('userInfoAuto error:', err)
+    });
+  },
+  ownerNameInput(e) {
+    this.setData({
+      [`form.ownerName`]: e.detail.value
+    });
+  },
+  ownerChildrenInput(e) {
+    this.setData({
+      [`form.ownerChildren`]: [ e.detail.value ]
+    });
+  },
+  ownerPhoneInput(e) {
+    this.setData({
+      [`form.ownerPhone`]: e.detail.value
+    });
   },
 });
